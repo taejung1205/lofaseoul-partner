0c3fa3eb4b15acc312cda27e07d82f5b6418913d
Taejung Kim <immcoc1@gmail.com>
2023-01-11 10:27:57 +0900

    feature: xlsx test

--- app/routes/admin/settlement-share.tsx
+++ app/routes/admin/settlement-share.tsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import styled from "styled-components";
+import * as xlsx from "xlsx";
+
+const SettlementSharePage = styled.div`
+  width: 100%;
+  font-size: 20px;
+  font-weight: 700;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  overflow-y: scroll;
+`;
+
 export default function AdminSettlementShare() {
-    return <>
-        <p>정산내역 공유</p>
-    </>
+  const [items, setItems] = useState<any>();
+
+  const readExcel = (e: any) => {
+    console.log("start");
+    e.preventDefault();
+    let json: any;
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        json = xlsx.utils.sheet_to_json(worksheet);
+        setItems(json);
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  };
+
+  return (
+    <SettlementSharePage>
+      <input type="file" onChange={readExcel} />
+      {items !== undefined
+        ? items.map((item: any, index: number) => {
+            console.log(item);
+            return <div>{item.업체명}</div>;
+          })
+        : "tt"}
+      <button onClick={() => console.log(items.length)}>xx</button>
+    </SettlementSharePage>
+  );
 }
