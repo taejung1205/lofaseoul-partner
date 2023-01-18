0122adb1545fa3046b13fb0a025356a12f21fc38
Taejung Kim <immcoc1@gmail.com>
2023-01-18 18:16:59 +0900

    refactor: seller select as component

--- app/components/seller.tsx
+++ app/components/seller.tsx
@@ -1,1 +1,47 @@
-export const PossibleSellers = ["29cm", "EQL", "로파공홈", "오늘의집", "카카오"];
+import { Select } from "@mantine/core";
+
+export const PossibleSellers = [
+  "29cm",
+  "EQL",
+  "로파공홈",
+  "오늘의집",
+  "카카오",
+];
+
+export function SellerSelect({
+  seller,
+  setSeller,
+}: {
+  seller: string;
+  setSeller: (value: string) => void;
+}) {
+  return (
+    <Select
+      value={seller}
+      onChange={setSeller}
+      data={[
+        { value: "all", label: "전체 판매처" },
+        { value: "29cm", label: "29cm" },
+        { value: "EQL", label: "EQL" },
+        { value: "로파공홈", label: "로파 홈페이지" },
+        { value: "오늘의집", label: "오늘의집" },
+        { value: "카카오", label: "카카오" },
+        { value: "etc", label: "기타" },
+      ]}
+      styles={{
+        input: {
+          fontSize: "20px",
+          fontWeight: "bold",
+          borderRadius: 0,
+          border: "3px solid black !important",
+          height: "40px",
+        },
+        item: {
+          "&[data-selected]": {
+            backgroundColor: "grey",
+          },
+        },
+      }}
+    />
+  );
+}
--- app/routes/admin/settlement-manage.tsx
+++ app/routes/admin/settlement-manage.tsx
@@ -1,7 +1,15 @@
 import { json, LoaderFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { useEffect, useMemo, useState } from "react";
 import { redirect } from "react-router";
 import styled from "styled-components";
-import { monthToNumeral, numeralMonthToKorean } from "~/components/date";
+import {
+  MonthSelectPopover,
+  monthToKorean,
+  monthToNumeral,
+  numeralMonthToKorean,
+} from "~/components/date";
+import { SellerSelect as SellerSelect } from "~/components/seller";
 import { getAllSettlementSum } from "~/services/firebase.server";
 
 const SettlementManagePage = styled.div`
@@ -15,27 +23,99 @@ const SettlementManagePage = styled.div`
   overflow-y: scroll;
 `;
 
+const GetListButton = styled.button`
+  background-color: white;
+  border: 3px solid black;
+  font-size: 20px;
+  font-weight: 700;
+  width: 110px;
+  height: 40px;
+  line-height: 1;
+  margin-left: 20px;
+  padding: 6px 6px 6px 6px;
+  cursor: pointer;
+`;
+
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const month = url.searchParams.get("month");
-  if (month == null) {
-    const today = new Date();
-    const todayMonthNumeral = monthToNumeral(today);
-    return redirect(`/admin/settlement-manage?month=${todayMonthNumeral}`);
-  }
 
-  const monthStr = numeralMonthToKorean(month);
-  const sums = await getAllSettlementSum({
-    monthStr: monthStr,
-  });
-  console.log(sums);
-  return json({ sums: sums });
+  if (month !== null) {
+    const monthStr = numeralMonthToKorean(month);
+    const sums = await getAllSettlementSum({
+      monthStr: monthStr,
+    });
+    console.log(sums);
+    return json({ sums: sums });
+  } else {
+    return null;
+  }
 };
 
 export default function AdminSettlementManage() {
+  const [selectedDate, setSelectedDate] = useState<Date>();
+  const [selectedMonthStr, setSelectedMonthStr] = useState<string>();
+  const [seller, setSeller] = useState<string>("all");
+  const loaderData = useLoaderData();
+
+  const monthNumeral = useMemo(
+    () => monthToNumeral(selectedDate ?? new Date()),
+    [selectedDate]
+  );
+
+  const sums = useMemo(() => {
+    if (loaderData == null) {
+      return null;
+    } else {
+      return loaderData.sums;
+    }
+  }, [loaderData]);
+
+  useEffect(() => {
+    setSelectedDate(new Date());
+  }, []);
+
+  useEffect(() => {
+    if (selectedDate !== undefined) {
+      setSelectedMonthStr(monthToKorean(selectedDate));
+    }
+  }, [selectedDate]);
+
   return (
     <SettlementManagePage>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "inherit",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img src="/images/icon_calendar.svg" />
+          <MonthSelectPopover
+            onLeftClick={() =>
+              setSelectedDate(
+                new Date(selectedDate!.setMonth(selectedDate!.getMonth() - 1))
+              )
+            }
+            onRightClick={() =>
+              setSelectedDate(
+                new Date(selectedDate!.setMonth(selectedDate!.getMonth() + 1))
+              )
+            }
+            monthStr={selectedMonthStr ?? ""}
+          />
+          <Link to={`/partner/settlement-manage?month=${monthNumeral}`}>
+            <GetListButton type="submit">조회하기</GetListButton>
+          </Link>
+        </div>
 
+        <SellerSelect
+          seller={seller}
+          setSeller={setSeller}
+        />
+      </div>
     </SettlementManagePage>
   );
 }
