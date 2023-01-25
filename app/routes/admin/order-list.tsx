2fe85035b458578035a1386d3512c68f234cd000
Taejung Kim <immcoc1@gmail.com>
2023-01-26 00:02:14 +0900

    feature: order-list page

--- app/routes/admin/order-list.tsx
+++ app/routes/admin/order-list.tsx
@@ -1,56 +1,219 @@
 import { PageLayout } from "~/components/page_layout";
 
 import dayPickerStyles from "react-day-picker/dist/style.css";
-import { useEffect, useMemo, useState } from "react";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Link, useLoaderData, useSubmit } from "@remix-run/react";
 import {
   dateToDayStr,
   DaySelectPopover,
   dayStrToDate,
 } from "~/components/date";
 import { GetListButton } from "~/components/button";
-import { json, LoaderFunction } from "@remix-run/node";
+import {
+  ActionFunction,
+  json,
+  LoaderFunction,
+  redirect,
+} from "@remix-run/node";
+import { BasicModal, ModalButton } from "~/components/modal";
+import { OrderItem, OrderTable } from "~/components/order";
+import styled from "styled-components";
+import { deleteOrders, getAllOrders } from "~/services/firebase.server";
+
+const EmptySettlementBox = styled.div`
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  height: 100px;
+  align-items: center;
+  justify-content: center;
+  width: inherit;
+`;
+
+const DeleteButton = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 24px;
+  font-weight: 700;
+  width: 220px;
+  height: 50px;
+  line-height: 1;
+  padding: 6px 6px 6px 6px;
+  margin-right: 40px;
+  cursor: pointer;
+`;
 
 export function links() {
   return [{ rel: "stylesheet", href: dayPickerStyles }];
 }
 
-export const loader: LoaderFunction = async ({ request }) => {
-    const url = new URL(request.url);
-    const day = url.searchParams.get("day");
-  
-    if (day !== null) {
-      //   const monthStr = numeralMonthToKorean(month);
-      //   const sums = await getAllSettlementSum({
-      //     monthStr: monthStr,
-      //   });
-      //   console.log(sums);
-      return json({ day: day });
-    } else {
-      return null;
+export const action: ActionFunction = async ({ request }) => {
+  const body = await request.formData();
+  const actionType = body.get("action")?.toString();
+  if (actionType === "delete") {
+    const orders = body.get("order")?.toString();
+    const day = body.get("day")?.toString();
+    if (orders !== undefined && day !== undefined) {
+      const jsonArr: OrderItem[] = JSON.parse(orders);
+      await deleteOrders({
+        orders: jsonArr,
+        dayStr: day,
+      });
+      return redirect(encodeURI(`/admin/order-list?day=${day}`));
     }
-  };
+  }
+
+  return null;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const day = url.searchParams.get("day");
+  console.log(day);
+
+  if (day !== null) {
+    const orders = await getAllOrders(day);
+    return json({ day: day, orders: orders });
+  } else {
+    const today = dateToDayStr(new Date());
+    const orders = await getAllOrders(today);
+    return json({ day: today, orders: orders });
+  }
+};
 
 export default function AdminOrderList() {
   const [selectedDate, setSelectedDate] = useState<Date>();
+  const [errorModalStr, setErrorModalStr] = useState<string>(""); //안내 모달창에서 뜨는 메세지
+  const [selectedItems, setSelectedItems] = useState<OrderItem[]>([]); // 체크박스로 선택된 아이템 목록. 삭제, 수정 버튼 눌렀을 때 업데이트됨
+  const [itemsChecked, setItemsChecked] = useState<boolean[]>([]); //체크된 정산내역 index 배열
+  const [items, setItems] = useState<OrderItem[]>([]); //로딩된 전체 정산내역 아이템 리스트
+
+  const [isDeleteModalOpened, setIsDeleteModalOpened] =
+    useState<boolean>(false);
+  const [isErrorModalOpened, setIsErrorModalOpened] = useState<boolean>(false);
+
+  const submit = useSubmit();
   const loaderData = useLoaderData();
+  const formRef = useRef<HTMLFormElement>(null);
 
   const selectedDayStr = useMemo(
     () => dateToDayStr(selectedDate ?? new Date()),
     [selectedDate]
   );
 
+  //loaderData에서 불러온 에러 정보를 바탕으로 한 에러 메세지
+  const errorOrderStr = useMemo(() => {
+    if (loaderData.error == undefined) {
+      return null;
+    }
+
+    switch (loaderData.error) {
+      case "day null":
+        return `날짜 정보가 잘못되었습니다. 다시 조회해주세요.`;
+    }
+    return "알 수 없는 오류입니다.";
+  }, [loaderData]);
+
   //날짜 수신
   useEffect(() => {
-    if (loaderData == null) {
+    if (loaderData.error !== undefined) {
       setSelectedDate(new Date());
     } else {
       setSelectedDate(dayStrToDate(loaderData.day));
     }
   }, []);
 
+  useEffect(() => {
+    if (loaderData.error == undefined) {
+      setItems(loaderData.orders);
+    }
+  }, [loaderData]);
+
+  //정산건 삭제를 post합니다.
+  function submitDelete(orderList: OrderItem[]) {
+    const json = JSON.stringify(orderList);
+    const formData = new FormData(formRef.current ?? undefined);
+    formData.set("order", json);
+    formData.set("day", loaderData.day);
+    formData.set("action", "delete");
+    submit(formData, { method: "post" });
+  }
+
+  //체크박스로 선택된 정산내역을 업뎃합니다. (삭제, 수정 버튼 클릭시 발생)
+  // 수정된 리스트를 반환합니다.
+  function updateCheckedItems() {
+    let settlementList = [];
+    for (let i = 0; i < items.length; i++) {
+      if (itemsChecked[i]) {
+        settlementList.push(items[i]);
+      }
+    }
+    setSelectedItems(settlementList);
+    return settlementList;
+  }
+
+  function onItemCheck(index: number, isChecked: boolean) {
+    itemsChecked[index] = isChecked;
+  }
+
+  function onCheckAll(isChecked: boolean) {
+    setItemsChecked(Array(items.length ?? 0).fill(isChecked));
+  }
+
   return (
     <>
+      {/* 안내용 모달 */}
+      <BasicModal
+        opened={isErrorModalOpened}
+        onClose={() => setIsErrorModalOpened(false)}
+      >
+        <div
+          style={{
+            justifyContent: "center",
+            textAlign: "center",
+            fontWeight: "700",
+          }}
+        >
+          {errorModalStr}
+          <div style={{ height: "20px" }} />
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <ModalButton onClick={() => setIsErrorModalOpened(false)}>
+              확인
+            </ModalButton>
+          </div>
+        </div>
+      </BasicModal>
+
+      {/* 정산내역 삭제 모달 */}
+      <BasicModal
+        opened={isDeleteModalOpened}
+        onClose={() => setIsDeleteModalOpened(false)}
+      >
+        <div
+          style={{
+            justifyContent: "center",
+            textAlign: "center",
+            fontWeight: "700",
+          }}
+        >
+          {`선택된 정산내역 ${selectedItems.length}건을 삭제하시겠습니까?`}
+          <div style={{ height: "20px" }} />
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <ModalButton onClick={() => setIsDeleteModalOpened(false)}>
+              취소
+            </ModalButton>
+            <ModalButton
+              onClick={async () => {
+                submitDelete(selectedItems);
+                setIsDeleteModalOpened(false);
+              }}
+            >
+              삭제
+            </ModalButton>
+          </div>
+        </div>
+      </BasicModal>
+
       <PageLayout>
         <div style={{ display: "flex", alignItems: "center" }}>
           <img src="/images/icon_calendar.svg" />
@@ -58,10 +221,44 @@ export default function AdminOrderList() {
             selectedDate={selectedDate}
             setSelectedDate={setSelectedDate}
           />
-          <Link to={`/admin/order-share?day=${selectedDayStr}`}>
+          <Link to={`/admin/order-list?day=${selectedDayStr}`}>
             <GetListButton>조회하기</GetListButton>
           </Link>
         </div>
+        <div style={{ height: "20px" }} />
+        {loaderData.error == undefined ? (
+          items.length > 0 ? (
+            <>
+              <OrderTable
+                items={items}
+                itemsChecked={itemsChecked}
+                onItemCheck={onItemCheck}
+                onCheckAll={onCheckAll}
+                defaultAllCheck={false}
+              />
+              <div style={{ height: "20px" }} />
+              <DeleteButton
+                onClick={() => {
+                  const updatedList = updateCheckedItems();
+                  if (updatedList.length > 0) {
+                    setIsDeleteModalOpened(true);
+                  } else {
+                    setErrorModalStr("선택된 정산내역이 없습니다.");
+                    setIsErrorModalOpened(true);
+                  }
+                }}
+              >
+                선택 주문서 삭제
+              </DeleteButton>
+            </>
+          ) : (
+            <EmptySettlementBox>
+              정산내역이 존재하지 않습니다.
+            </EmptySettlementBox>
+          )
+        ) : (
+          <EmptySettlementBox>{errorOrderStr}</EmptySettlementBox>
+        )}
       </PageLayout>
     </>
   );
