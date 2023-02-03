cb3c72eee92af1f0a4f144daaba76684bcf5c545
Taejung Kim <immcoc1@gmail.com>
2023-02-03 17:35:56 +0900

    feature: send delay message

--- app/routes/admin/delayed-order.tsx
+++ app/routes/admin/delayed-order.tsx
@@ -1,12 +1,16 @@
 import { PageLayout } from "~/components/page_layout";
 
-import { useEffect, useMemo, useState } from "react";
-import { useLoaderData, useSubmit } from "@remix-run/react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useActionData, useLoaderData, useSubmit } from "@remix-run/react";
 import { ActionFunction, json, LoaderFunction } from "@remix-run/node";
 import { BasicModal, ModalButton } from "~/components/modal";
 import { OrderItem, OrderTable } from "~/components/order";
 import styled from "styled-components";
-import { getAllDelayedOrders } from "~/services/firebase.server";
+import {
+  getAllDelayedOrders,
+  getPartnerProfile,
+} from "~/services/firebase.server";
+import { sendAligoMessage } from "~/services/aligo.server";
 
 const EmptySettlementBox = styled.div`
   display: flex;
@@ -32,7 +36,42 @@ const SendNoticeButton = styled.button`
 `;
 
 export const action: ActionFunction = async ({ request }) => {
-  //TODO: 알리고 메세지
+  try {
+    const body = await request.formData();
+    const actionType = body.get("action")?.toString();
+    if (actionType === "send") {
+      const receivers = body.get("receivers")?.toString();
+      if (receivers !== undefined) {
+        const receiversArr: string[] = JSON.parse(receivers);
+        for (let i = 0; receiversArr.length; i++) {
+          const profile: any = await getPartnerProfile({
+            name: receiversArr[i],
+          });
+          if (profile == null || profile.phone == undefined) {
+            throw Error("해당 파트너를 찾을 수 없습니다.");
+          } else {
+            const phone = profile.phone;
+            const response = await sendAligoMessage({
+              text: `[로파파트너] 현재 운송장 입력이 지연된 주문건이 있습니다. 확인 부탁드립니다.`,
+              receiver: phone,
+            });
+            if (
+              response.result_code !== undefined &&
+              response.result_code == 1
+            ) {
+              return json({ message: `메세지가 전송되었습니다.` });
+            } else {
+              throw Error(response.message);
+            }
+          }
+        }
+      }
+    }
+  } catch (error: any) {
+    return json({
+      message: `메세지 전송 중 오류가 발생했습니다. ${error.message ?? error}`,
+    });
+  }
 
   return null;
 };
@@ -43,17 +82,19 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function AdminDelayedOrder() {
-  const [errorModalStr, setErrorModalStr] = useState<string>(""); //안내 모달창에서 뜨는 메세지
+  const [noticeModalStr, setNoticeModalStr] = useState<string>(""); //안내 모달창에서 뜨는 메세지
   const [selectedItems, setSelectedItems] = useState<OrderItem[]>([]); // 체크박스로 선택된 아이템 목록. 삭제, 수정 버튼 눌렀을 때 업데이트됨
   const [itemsChecked, setItemsChecked] = useState<boolean[]>([]); //체크된 정산내역 index 배열
   const [items, setItems] = useState<OrderItem[]>([]); //로딩된 전체 정산내역 아이템 리스트
 
-  const [isDeleteModalOpened, setIsDeleteModalOpened] =
+  const [isSendModalOpened, setIsSendModalOpened] = useState<boolean>(false);
+  const [isNoticeModalOpened, setIsNoticeModalOpened] =
     useState<boolean>(false);
-  const [isErrorModalOpened, setIsErrorModalOpened] = useState<boolean>(false);
 
+  const actionData = useActionData();
   const submit = useSubmit();
   const loaderData = useLoaderData();
+  const formRef = useRef<HTMLFormElement>(null);
 
   //loaderData에서 불러온 에러 정보를 바탕으로 한 에러 메세지
   const errorOrderStr = useMemo(() => {
@@ -71,6 +112,13 @@ export default function AdminDelayedOrder() {
       setItems(loaderData.orders);
     }
   }, [loaderData]);
+
+  useEffect(() => {
+    if (actionData !== undefined && actionData !== null) {
+      setNoticeModalStr(actionData.message);
+      setIsNoticeModalOpened(true);
+    }
+  }, [actionData]);
 
   //체크박스로 선택된 정산내역을 업뎃합니다. (삭제, 수정 버튼 클릭시 발생)
   // 수정된 리스트를 반환합니다.
@@ -93,12 +141,35 @@ export default function AdminDelayedOrder() {
     setItemsChecked(Array(items.length ?? 0).fill(isChecked));
   }
 
+  function sendMessage() {
+    const receiverList: string[] = [];
+
+    for (let i = 0; i < items.length; i++) {
+      if (itemsChecked[i]) {
+        const partner = items[i].partnerName;
+        if (!receiverList.includes(partner)) {
+          receiverList.push(partner);
+        }
+      }
+    }
+
+    if (receiverList.length == 0) {
+      setNoticeModalStr("수신 대상이 없습니다.");
+      setIsNoticeModalOpened(true);
+      return null;
+    }
+    const formData = new FormData(formRef.current ?? undefined);
+    formData.set("receivers", JSON.stringify(receiverList));
+    formData.set("action", "send");
+    submit(formData, { method: "post" });
+  }
+
   return (
     <>
       {/* 안내용 모달 */}
       <BasicModal
-        opened={isErrorModalOpened}
-        onClose={() => setIsErrorModalOpened(false)}
+        opened={isNoticeModalOpened}
+        onClose={() => setIsNoticeModalOpened(false)}
       >
         <div
           style={{
@@ -107,10 +178,10 @@ export default function AdminDelayedOrder() {
             fontWeight: "700",
           }}
         >
-          {errorModalStr}
+          {noticeModalStr}
           <div style={{ height: "20px" }} />
           <div style={{ display: "flex", justifyContent: "center" }}>
-            <ModalButton onClick={() => setIsErrorModalOpened(false)}>
+            <ModalButton onClick={() => setIsNoticeModalOpened(false)}>
               확인
             </ModalButton>
           </div>
@@ -119,8 +190,8 @@ export default function AdminDelayedOrder() {
 
       {/* 알림 발송 모달 */}
       <BasicModal
-        opened={isDeleteModalOpened}
-        onClose={() => setIsDeleteModalOpened(false)}
+        opened={isSendModalOpened}
+        onClose={() => setIsSendModalOpened(false)}
       >
         <div
           style={{
@@ -132,13 +203,13 @@ export default function AdminDelayedOrder() {
           {`선택된 주문 ${selectedItems.length}건에 대한 알림을 발송하시겠습니까?`}
           <div style={{ height: "20px" }} />
           <div style={{ display: "flex", justifyContent: "center" }}>
-            <ModalButton onClick={() => setIsDeleteModalOpened(false)}>
+            <ModalButton onClick={() => setIsSendModalOpened(false)}>
               취소
             </ModalButton>
             <ModalButton
               onClick={async () => {
-                //TODO: 알리고 메세지
-                setIsDeleteModalOpened(false);
+                sendMessage();
+                setIsSendModalOpened(false);
               }}
             >
               발송
@@ -171,10 +242,10 @@ export default function AdminDelayedOrder() {
                   onClick={() => {
                     const updatedList = updateCheckedItems();
                     if (updatedList.length > 0) {
-                      setIsDeleteModalOpened(true);
+                      setIsSendModalOpened(true);
                     } else {
-                      setErrorModalStr("선택된 정산내역이 없습니다.");
-                      setIsErrorModalOpened(true);
+                      setNoticeModalStr("선택된 정산내역이 없습니다.");
+                      setIsNoticeModalOpened(true);
                     }
                   }}
                 >
