0ac6d5f623dd5acd8d70d0041d614e14eaec9200
Taejung Kim <immcoc1@gmail.com>
2023-07-07 17:39:52 +0900

    fix: remove remix-esbuild

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,3 @@
-const { withEsbuildOverride } = require("remix-esbuild-override");
-const styledComponentsPlugin = require("./styled-components-esbuild-plugin");
-
-withEsbuildOverride((option) => {
-  option.plugins.unshift(styledComponentsPlugin());
-
-  return option;
-});
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
