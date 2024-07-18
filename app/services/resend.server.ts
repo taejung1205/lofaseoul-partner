7519a8b6e03ab2193086d7a7f7a88bff44bac054
Taejung Kim <immcoc1@gmail.com>
2024-07-18 14:26:37 +0900

    test: send multiple mails

--- app/services/resend.server.ts
+++ app/services/resend.server.ts
@@ -3,6 +3,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_KEY);
 
+
 export async function sendResendEmail({
   to,
   subject,
@@ -13,11 +14,17 @@ export async function sendResendEmail({
   html: string;
 }) {
   const result = await resend.emails.send({
-    from: "kyj@tabacpress.xyz",
+    from: "tabacpress<kyj@tabacpress.xyz>",
     to: to,
     subject: subject,
     html: html,
   });
 
+  return result;
+}
+
+//mailList: {from: string; to: string[]; subject: string; html: string;}
+export async function sendResendBatchEmail({ mailList }: { mailList: any[] }) {
+  const result = await resend.batch.send(mailList);
   return result;
 }
