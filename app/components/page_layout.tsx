72f47c7cfd8c6d39d1922cd16fc1c703ace8c56a
Taejung Kim <immcoc1@gmail.com>
2024-07-24 17:41:43 +0900

    fix: page layout & profile font size

--- app/components/page_layout.tsx
+++ app/components/page_layout.tsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 export const PageLayout = styled.div<{isMobile?: boolean}>`
   width: 100%;
+  height: 100%;
   font-size: 20px;
   font-weight: 700;
   padding: 30px 40px 30px 40px;
   padding-bottom: ${(props) => (props.isMobile ? "100px" : "30px")};
-  display: flex;
+  display: ${(props) => (props.isMobile ? "" : "flex")};
   flex-direction: column;
   align-items: flex-start;
   overflow-y: scroll;
