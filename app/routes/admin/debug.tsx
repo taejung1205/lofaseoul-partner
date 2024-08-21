5431df940c35848769f298d89bb588195c28cba9
Taejung Kim <immcoc1@gmail.com>
2024-08-21 15:57:45 +0900

    fix: move debug functions to other file

--- app/routes/admin/debug.tsx
+++ app/routes/admin/debug.tsx
@@ -1,11 +1,11 @@
 import { ActionFunction } from "@remix-run/node";
 import { useSubmit } from "@remix-run/react";
 import { useRef } from "react";
-import { debug_fixPartnerProviderNameStandard } from "~/services/firebase.server";
+import { debug_initializeIsDiscountedForSettlements } from "~/services/firebase-debug.server";
 
 export const action: ActionFunction = async ({ request }) => {
   console.log("request submitted");
-  debug_fixPartnerProviderNameStandard();
+  debug_initializeIsDiscountedForSettlements();
   return null;
 };
 
